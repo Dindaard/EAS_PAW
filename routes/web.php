3e1ffc41f98d38e3cbb46198ce4a04b28d801bcf
Dindaard <dindaard27@gmail.com>
2022-06-29 15:14:13 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
 });
+
+Route::get('/beranda', function () {
+    return view('beranda');
+});
